6b7e71fe30f012c81aa1f6130dd364e63760a09a
Pankaj Sharma <pankaj@alefeducation.com>
2018-10-20 16:37:47 +0400

    [#1] Add equal hashcode to customer

--- core/src/main/kotlin/com/pankaj/shoppingcart/command/model/Customer.kt
+++ core/src/main/kotlin/com/pankaj/shoppingcart/command/model/Customer.kt
@@ -6,6 +6,20 @@ class Customer(private val email: Email) : Aggregate<CustomerId> {
     override fun id(): CustomerId = CustomerId()
 
     fun hasEmail(email: String): Boolean = this.email == Email(email)
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        other as Customer
+        if (id() != other.id()) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id().hashCode()
+    }
+
+
 }
 
 data class CustomerId(val id: String = randomUUID().toString()) : Id
