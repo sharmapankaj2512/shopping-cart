fd976c0bf63fd0f02389f5edda10fc0140fa6e39
Pankaj Sharma <pankaj@alefeducation.com>
2018-10-21 07:18:54 +0400

    [#1] Add exists method to in memory repository

--- infrastructure/in-memory-command-persistence/src/main/kotlin/com/pankaj/shoppingcart/command/repositories/InMemoryCustomerRepository.kt
+++ infrastructure/in-memory-command-persistence/src/main/kotlin/com/pankaj/shoppingcart/command/repositories/InMemoryCustomerRepository.kt
@@ -2,13 +2,15 @@ package com.pankaj.shoppingcart.command.repositories
 
 import com.pankaj.shoppingcart.command.model.Customer
 import com.pankaj.shoppingcart.command.model.Email
+import com.pankaj.shoppingcart.command.usecases.registerCustomer.CustomerExistsRepository
 import com.pankaj.shoppingcart.command.usecases.repositories.CreateCustomerRepository
 import org.springframework.stereotype.Repository
 import reactor.core.publisher.Mono
 
 @Repository
-class InMemoryCustomerRepository : CreateCustomerRepository {
+class InMemoryCustomerRepository : CreateCustomerRepository, CustomerExistsRepository {
     companion object {
+
         val customers = mutableMapOf<Email, Customer>()
     }
 
@@ -16,4 +18,6 @@ class InMemoryCustomerRepository : CreateCustomerRepository {
         customers[customer.email()] = customer
         return Mono.just(customer)
     }
+
+    override fun exists(email: Email): Boolean = customers.containsKey(email)
 }
